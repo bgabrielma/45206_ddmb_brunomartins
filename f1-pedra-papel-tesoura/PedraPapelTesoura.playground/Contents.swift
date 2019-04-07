3a42e833697071a59ada9b14eb6cc0fd8f204d03
Bruno Gabriel Nunes Martins <brunonunes.martins@my.istec.pt>
2019-04-07 09:53:59 -0700

    Finish exercise

--- f1-pedra-papel-tesoura/PedraPapelTesoura.playground/Contents.swift
+++ f1-pedra-papel-tesoura/PedraPapelTesoura.playground/Contents.swift
@@ -1,3 +1,45 @@
 import UIKit
 
-var str = "Hello, playground"
+// Pedra Papel Tesoura
+//
+//
+// To ISTEC
+//
+//
+// brunonunes.martins@my.istec.pt - Bruno Martins - DDMB - 45206
+
+// Funcionamento:
+//
+/*
+ *       [Atributo] |    Pedra   |   Papel   |   Tesoura
+ *   ---------------|-----------------------------------
+ *   [Ganha a quem?] |    Tesoura |   Pedra   |   Papel
+ *
+ */
+
+var mecanismoJogo:[[String]] = [["pedra", "tesoura"], ["papel", "pedra"], ["tesoura", "papel"]]
+let opcoes:[String] = ["pedra", "papel", "tesoura"]
+
+let escolhaMaquina = opcoes[1]
+let escolhaUser = opcoes[0]
+
+
+print("O utilizador escolhe: \(escolhaUser)")
+print ("A máquina escolhe: \(escolhaMaquina)")
+
+print("\n-----Resultado-----")
+print("Empate: \(escolhaUser == escolhaMaquina ? "sim" : "não") \n")
+
+for mecanismo in mecanismoJogo where escolhaUser != escolhaMaquina {
+    guard mecanismo[0] == escolhaMaquina else {
+        continue
+    }
+    
+    if(mecanismo[1] == escolhaUser) {
+        print("A máquina ganha!")
+    }
+    else {
+        print("O Utilizador ganha!")
+        break;
+    }
+}

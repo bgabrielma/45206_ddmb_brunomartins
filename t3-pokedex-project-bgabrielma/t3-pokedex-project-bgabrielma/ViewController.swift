a93a6499aa666de31d6970e12aebc911f0913375
Aluno_Istec <Aluno_istec@Mac-01s-Mac-mini-52.local>
2019-06-18 10:33:43 +0100

    Created inicial structure for this project

---
+++ t3-pokedex-project-bgabrielma/t3-pokedex-project-bgabrielma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  t3-pokedex-project-bgabrielma
+//
+//  Created by Aluno_Istec on 18/06/2019.
+//  Copyright © 2019 ISTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

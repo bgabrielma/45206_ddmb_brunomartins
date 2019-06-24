147fd197bce36a3863f99957a6948a57e4c43334
Developer <developer@Mac0405s-iMac.local>
2019-06-24 10:03:18 +0100

    Update files

---
+++ t3-pokedex-bgabrielma-v2/t3-pokedex-bgabrielma-v2/Classes/AppUtils.swift
@@ -0,0 +1,17 @@
+//
+//  AppUtils.swift
+//  t3-pokedex-bgabrielma-v2
+//
+//  Created by Developer on 24/06/2019.
+//  Copyright © 2019 Bruno Martins. All rights reserved.
+//
+
+import UIKit
+
+final class AppUtils: NSObject {
+    // find and return image resource
+    // return mechanism of types pokemon
+    
+    public static var typesRelation:[Attack] = []
+    
+}
